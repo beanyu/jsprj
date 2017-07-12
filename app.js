7cb590a37a90bf74c9b2a3645ca0b49d90a818cd
bean <bean.yu110@gmail.com>
2017-07-12 22:35:24 +0800

    react+koa demo
    react+koa demo

--- app.js
+++ app.js
@@ -1,36 +1,20 @@
 const Koa = require('koa');
 const route = require( 'koa-router' );
-const Pug = require( 'koa-pug' );
 
 const app = new Koa();
+//app.listen(3000);
 
-app.use(ctx => {
-  ctx.body = 'Hello World';
+
+const Pug = require('koa-pug');
+const pug = new Pug({
+  viewPath: './views',
+  app: app // equals to pug.use(app) and app.use(pug.middleware)
+});
+
+pug.locals.someKey = 'some value';
+
+app.use(function* () {
+  this.render('index', true)
 });
 
 app.listen(3000);
-
-const koa = require('koa')
-const app = koa()
-
-const Pug = require('koa-pug')
-const pug = new Pug({
-  viewPath: './views',
-  debug: false,
-  pretty: false,
-  compileDebug: false,
-  locals: global_locals_for_all_pages,
-  basedir: 'path/for/pug/extends',
-  helperPath: [
-    'path/to/pug/helpers',
-    { random: 'path/to/lib/random.js' },
-    { _: require('lodash') }
-  ],
-  app: app // equals to pug.use(app) and app.use(pug.middleware)
-})
-
-pug.locals.someKey = 'some value'
-
-app.use(function* () {
-  this.render('index', locals_for_this_page, true)
-})
